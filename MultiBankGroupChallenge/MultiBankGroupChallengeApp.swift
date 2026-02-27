e181252a25f71b3e4a431a7986bd17ebdf2299ef
Adilet <adoxa7@gmail.com>
2026-02-27 14:32:51 +0700

    Initial Commit

---
+++ MultiBankGroupChallenge/MultiBankGroupChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBankGroupChallengeApp.swift
+//  MultiBankGroupChallenge
+//
+//  Created by Mamirova Akbota on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiBankGroupChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
